62cec4195040eeee7e48283c6a0fe7cd8252df7a
Jason Ho <jasonzx.ho@gmail.com>
2020-11-04 10:45:53 -0500

    update to hello message and comments

--- helloworld.py
+++ helloworld.py
@@ -1,7 +1,9 @@
 def hello():
-	print("Hello world!")
+	print("Hello, world!")
 
 # this statement helps with running hello() whenever this
 # file is ran from the command line
 if __name__ == "__main__":
 	hello()
+
+# more comment
